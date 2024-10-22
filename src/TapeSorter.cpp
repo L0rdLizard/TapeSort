44eda4a7ee7b9761d5bb8554d852ef5b94a014cc
L0rdLizard <WarriorXKZ@gmail.com>
2024-10-22 20:27:31 +0300

    TapeSorter

--- src/TapeSorter.cpp
+++ src/TapeSorter.cpp
@@ -1,14 +1,103 @@
-#include <TapeSorter.h>
+#include "TapeSorter.h"
+#include <fstream>
+#include <algorithm>
 #include <iostream>
 
-TapeSorter::TapeSorter() {
+TapeSorter::TapeSorter(TapeDevice& inputTape, TapeDevice& outputTape, size_t memoryLimit)
+    : inputTape(inputTape), outputTape(outputTape), memoryLimit(memoryLimit) {}
 
+void TapeSorter::sort() {
+    size_t chunkSize = memoryLimit / sizeof(int);
+    std::vector<int> buffer;
+
+    std::vector<std::string> tempFiles;
+
+    while (true) {
+        buffer.clear();
+        try {
+            for (size_t i = 0; i < chunkSize && inputTape.getCurrentPosition() < inputTape.getLength(); ++i) {
+                buffer.push_back(inputTape.getCurrentCell());
+                inputTape.moveToNextCell();
+            }
+            if (buffer.empty()) break;
+
+            std::sort(buffer.begin(), buffer.end());
+
+            std::string tempFileName = createTempFile(buffer);
+            tempFiles.push_back(tempFileName);
+        }
+        catch (const std::out_of_range&) {
+            break;
+        }
+    }
+    mergeTempFiles(tempFiles);
+
+    for (const std::string& tempFile : tempFiles) {
+        std::remove(tempFile.c_str());
+    }
 }
 
-TapeSorter::~TapeSorter() {
-    
+std::string TapeSorter::createTempFile(const std::vector<int>& buffer) {
+    static int tempFileIndex = 0;
+    std::string tempFileName = "../tmp/temp_tape_" + std::to_string(tempFileIndex++) + ".dat";
+
+    std::ofstream tempFile(tempFileName, std::ios::binary);
+    if (!tempFile) {
+        throw std::runtime_error("Failed to create temporary file");
+    }
+
+    for (int value : buffer) {
+        tempFile.write(reinterpret_cast<const char*>(&value), sizeof(int));
+    }
+
+    tempFile.close();
+    return tempFileName;
 }
 
-void TapeSorter::sortTape(TapeDevice& d1, TapeDevice& d2) {
-    std::cout << "1";
+void TapeSorter::mergeTempFiles(const std::vector<std::string>& tempFiles) {
+    std::vector<std::ifstream> tempStreams(tempFiles.size());
+    for (size_t i = 0; i < tempFiles.size(); ++i) {
+        tempStreams[i].open(tempFiles[i], std::ios::binary);
+        if (!tempStreams[i]) {
+            throw std::runtime_error("Failed to open temporary file: " + tempFiles[i]);
+        }
+    }
+
+    struct TapeElement {
+        int value;
+        size_t fileIndex;
+        bool operator>(const TapeElement& other) const {
+            return value > other.value;
+        }
+    };
+
+    std::priority_queue<TapeElement, std::vector<TapeElement>, std::greater<TapeElement>> minHeap;
+
+    for (size_t i = 0; i < tempFiles.size(); ++i) {
+        int value;
+        tempStreams[i].read(reinterpret_cast<char*>(&value), sizeof(int));
+        if (tempStreams[i]) {
+            minHeap.push({ value, i });
+        }
+    }
+
+    while (!minHeap.empty()) {
+        TapeElement smallest = minHeap.top();
+        minHeap.pop();
+
+        outputTape.changeCurrentCell(smallest.value);
+        if (outputTape.getCurrentPosition() < outputTape.getLength() - 1) {
+            outputTape.moveToNextCell();
+        }
+
+        int value;
+        tempStreams[smallest.fileIndex].read(reinterpret_cast<char*>(&value), sizeof(int));
+        if (tempStreams[smallest.fileIndex]) {
+            minHeap.push({ value, smallest.fileIndex });
+        }
+    }
+
+    for (auto& tempStream : tempStreams) {
+        tempStream.close();
+    }
 }
--- src/TapeSorter.h
+++ src/TapeSorter.h
@@ -2,12 +2,22 @@
 #define TAPESORTER_H
 
 #include "TapeDevice.h"
+#include <vector>
+#include <string>
+#include <queue>
+#include <stdexcept>
 
 class TapeSorter {
 public:
-    TapeSorter();
-    void sortTape(TapeDevice& d1, TapeDevice& d2);
-    ~TapeSorter();
+    TapeSorter(TapeDevice& inputTape, TapeDevice& outputTape, size_t memoryLimit);
+    void sort();
+private:
+    TapeDevice& inputTape;
+    TapeDevice& outputTape;
+    size_t memoryLimit;
+
+    std::string createTempFile(const std::vector<int>& buffer);
+    void mergeTempFiles(const std::vector<std::string>& tempFiles);
 };
 
 #endif // TAPESORTER_H
