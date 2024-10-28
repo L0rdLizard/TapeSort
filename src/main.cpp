802c89d3dda7090e38ea83399122dba412d0de75
L0rdLizard <WarriorXKZ@gmail.com>
2024-10-28 17:27:24 +0300

    delete func in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,6 @@
 #include <chrono>
 #include "Config.h"
 
-void testTime()
-{
-    try
-    {
-        TimeManager timeManager(false);
-        
-        std::string configFilename = "../config/delays.cfg";
-        Config config(configFilename);
-        std::unordered_map<std::string, int> delays = config.loadConfig();
-
-        TapeDevice tape1("tape3", delays);
-
-        TapeDevice tape2("tape4", tape1.getLength(), delays);
-
-        size_t memorySize = 64;
-
-        TapeSorter tapeSorter(tape1, tape2, memorySize, timeManager, delays);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        tapeSorter.sort();
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> duration = end - start;
-        std::cout << "Real time: " << duration.count() << " seconds." << std::endl;
-
-        std::cout << "Model time with delays: " << timeManager.get_global_execution_time() << " ms" << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-}
-
 int main(int argc, char* argv[])
 {   
     if (argc != 3) {
